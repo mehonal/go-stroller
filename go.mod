56ec634be37425b958e85fd1a0a56aacb91d1f05
Mehonal <116497904+mehonal@users.noreply.github.com>
2024-11-14 23:30:26 +0300

    improved scrapeWebResults such that it returns title and descriptions as slice

--- go.mod
+++ go.mod
@@ -15,8 +15,10 @@ require (
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.31.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gocolly/colly"
 )
 
-func scrapeWebResults(keyword string) (err error){
+func scrapeWebResults(keyword string) (titles []string, descriptions []string, err error){
     var url string = "https://www.google.com/search?q=" + keyword + "&num=" + fmt.Sprintf("%d", 100) 
     c := colly.NewCollector(
         colly.AllowedDomains("google.com"),
@@ -14,18 +14,30 @@ func scrapeWebResults(keyword string) (err error){
     )
     c.OnHTML(".egMi0 h3", func(e *colly.HTMLElement) { // all titles; other useful ones may be: kCrYT and egMi0
         fmt.Print("Title: " + e.Text + "\n")
+        if (len(titles) < 100) {
+            titles = append(titles, e.Text)
+        }
     })
     c.OnHTML(".BNeawe.s3v9rd.AP7Wnd", func(e *colly.HTMLElement) { // all descriptions
         fmt.Print("Desc: " + e.Text + "\n") // e.DOM.Html() if HTML is needed
+        if (len(descriptions) < 100) {
+            descriptions = append(descriptions, e.Text)
+        }
     })
     err = c.Visit(url)
     if err != nil {
         fmt.Print(err)
     }
-    return err
+    return titles, descriptions, err
 }
 
 func main() {
-    // scrapeWebResults("golang")
-    // fmt.Printf("Environment variable: %s\n", os.Getenv("TEST"))
+    fmt.Print("Running Stroller...")
+    titles, descriptions, err := scrapeWebResults("golang")
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Titles: %v\n", titles)
+    fmt.Printf("Descriptions: %v\n", descriptions)
+    fmt.Printf("Environment variable: %s\n", os.Getenv("TEST"))
 }
